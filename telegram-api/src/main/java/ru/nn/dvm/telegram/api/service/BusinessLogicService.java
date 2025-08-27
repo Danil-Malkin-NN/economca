d548775676d7347f2410414a6ed8bbf945922dff
dvmalkin <dvmalkin@phenixit.ru>
2025-08-27 14:32:40 +0300

    Удалил лишнее из ги

--- telegram-api/src/main/java/ru/nn/dvm/telegram/api/service/BusinessLogicService.java
+++ telegram-api/src/main/java/ru/nn/dvm/telegram/api/service/BusinessLogicService.java
@@ -4,23 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import ru.nn.dvm.core.service.EconomService;
 import ru.nn.dvm.telegram.api.service.steps.StepService;
 
 @Service
 @RequiredArgsConstructor
 public class BusinessLogicService {
 
-	private final EconomService economService;
 	private final StepAggregator aggregator;
 
 	public SendMessage processAction(Update update) {
 
 		StepService service = aggregator.getStepProcessor(update);
 
-		SendMessage message = service.process(update);
-
-
-		return message;
+		return service.process(update);
 	}
 }
